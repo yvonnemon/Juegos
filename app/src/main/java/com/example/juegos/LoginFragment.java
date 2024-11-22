67f26d811efc31b1ffb1ac065aa148dc06d555c9
Yvonne <yvosobrero2@hotmail.com>
2024-11-22 21:07:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/juegos/LoginFragment.java
@@ -0,0 +1,17 @@
+package com.example.juegos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_fragment, container, false);  // Layout for this fragment
+    }}
